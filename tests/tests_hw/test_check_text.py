75ab41c8bc8a9e3ba236fc1870be34f1d616f407
Kirill Osadchiy <osadchiy.kirill@mail.ru>
2024-10-16 20:17:32 +0300

    test_check_text corrected

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,10 +1,7 @@
 from pages.demoqa import DemoQa
-from pages.elements_page import ElementsPage
-
 
 def test_check_text(browser):
     demo_qa_page = DemoQa(browser)
-    elements_page = ElementsPage(browser)
 
     demo_qa_page.visit()
     assert demo_qa_page.footer_text.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
