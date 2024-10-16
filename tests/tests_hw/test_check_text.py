a3afd76bdf757edc8ab8b245e28b82955a91970c
Kirill Osadchiy <osadchiy.kirill@mail.ru>
2024-10-16 20:12:47 +0300

    test_check_text corrected

--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -8,5 +8,5 @@ def test_check_text(browser):
 
     demo_qa_page.visit()
     assert demo_qa_page.footer_text.get_text() == '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
-    elements_page.visit()
+    demo_qa_page.btn_elements.click()
     assert demo_qa_page.elements_text.get_text() == 'Please select an item from left to start practice.'
